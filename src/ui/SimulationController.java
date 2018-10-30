4d60bb62bd549d801094089aa67607a1090e478d
philezwini <ssithungu@gmail.com>
2018-10-30 11:45:49 +0200

    Game can now pause and continue. Added toggle button for Training Mode. X axis of chart can now move to the right with time.

--- src/ui/SimulationController.java
+++ src/ui/SimulationController.java
@@ -3,7 +3,9 @@ package ui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import Entity.Ball;
 import Entity.Entity;
@@ -11,6 +13,7 @@ import Entity.Player;
 import ga.Selector;
 import game.GameState;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
@@ -29,9 +32,13 @@ public class SimulationController extends Controller {
 	private static Ball ball;
 	private static ArrayList<Player> players;
 	private GameState gState;
+	private boolean train;
+	private static ScheduledExecutorService es;
+
 	// Control variables for existing FXML nodes.
 	@FXML
 	private BorderPane rootPane;
+	private static BorderPane s_rootPane;
 
 	@FXML
 	private AnchorPane aPane;
@@ -40,26 +47,40 @@ public class SimulationController extends Controller {
 	private ImageView fieldView;
 
 	@FXML
-	private ToggleButton tGraphics;
+	private ToggleButton tTrain;
 
+	// The statitc fields are used to control the static fxml variables in a static
+	// way.
 	@FXML
-	private TextField tYScore, tBScore, tGen;
+	private TextField tYScore, tBScore, tGen, tMaxNumGen;
 	private static TextField s_tYScore, s_tBScore, s_tGen;
 
 	@FXML
-	private LineChart<String, Double> chart; // Chart for visualizing the average fitness over time.
+	private LineChart<String, Double> chart;
 	private static LineChart<String, Double> s_chart;
 
 	// Function for handing when the user clicks the "Close" menu item.
 	@FXML
 	private void closeMenuItemClick() {
-		// saveModels();
 		Stage s = (Stage) rootPane.getScene().getWindow();
-		closeWindow(s, true); // Call parent function that handles program // termination.
+		closeWindow(s, true); // Call parent function that handles program termination.
 	}
 
-	private void stopGame() {
-		gState.cancel();
+	// -- This method shuts down the executor service after the currently running
+	// thread finishes. --//
+	public static void stopGame(int status) {
+		if (status == 0) {
+			// Stop execution even though stopping conditions of the GA have not been met.
+			es.shutdown();
+		} else {
+			// Stop execution and notify the user that the stopping conditions of the GA
+			// have been met.
+			es.shutdown();
+			Platform.runLater(() -> {
+				Stage s = (Stage) s_rootPane.getScene().getWindow();
+				makeCustomAlert(s, "Training Complete.", "The stopping conditions of the algorithm have been met.");
+			});
+		}
 	}
 
 	@FXML
@@ -69,7 +90,7 @@ public class SimulationController extends Controller {
 
 	@FXML
 	private void btnStopClick() {
-		stopGame();
+		stopGame(0);
 	}
 
 	@FXML
@@ -79,18 +100,29 @@ public class SimulationController extends Controller {
 
 	@FXML
 	private void btnGraphicsClick() {
-		if (tGraphics.isSelected()) {
-			System.out.println("Graphics. On");
-			Entity.enableView(true);
-		} else {
-			System.out.println("Graphics Off");
+		if (tTrain.isSelected()) {
+			System.out.println("Train Mode On");
+			train = true;
 			Entity.enableView(false);
+		} else {
+			System.out.println("Train Mode Off");
+			train = false;
+			Entity.enableView(true);
 		}
 	}
 
 	private void startSim() {
-		// Schedule the GameState thread to run in fixed 20millisecond intervals.
-		new Timer().scheduleAtFixedRate(gState, 0, 20);
+		int maxNumGen = Integer.parseInt(tMaxNumGen.getText());
+		Selector.setNumGen(maxNumGen);
+
+		es = Executors.newScheduledThreadPool(1);
+		if (train) {
+			// The intervals between two executions must be shorter.
+			es.scheduleAtFixedRate(gState, 0, 500, TimeUnit.MICROSECONDS);
+		} else {
+			// The intervals should be longer so that the simulation can be watched..
+			es.scheduleAtFixedRate(gState, 0, 20, TimeUnit.MILLISECONDS);
+		}
 	}
 
 	@FXML
@@ -100,27 +132,34 @@ public class SimulationController extends Controller {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		Selector.initialize();
-		gState = new GameState(1000);
+		// -- Set the initial values for the text boxes. --//
+		tYScore.setText(0 + "");
+		tBScore.setText(0 + "");
+		tGen.setText(0 + "");
+		tMaxNumGen.setText(200 + "");
 
 		// --Make the static variables point to the same place as the original ones.--//
 		s_tYScore = tYScore;
 		s_tBScore = tBScore;
 		s_tGen = tGen;
 		s_chart = chart;
+		s_rootPane = rootPane;
 
-		s_tYScore.setText(gState.getyScore() + "");
-		s_tBScore.setText(gState.getbScore() + "");
-		s_tGen.setText(gState.getGeneration() + "");
-
+		// -- Create two series for the yellow and black team. --//
 		XYChart.Series<String, Double> ySeries = new Series<String, Double>();
 		XYChart.Series<String, Double> bSeries = new Series<String, Double>();
 		ySeries.setName("Team A");
 		bSeries.setName("Team B");
-
 		s_chart.getData().add(ySeries);
 		s_chart.getData().add(bSeries);
 
+		// --Initialise the program with the following parameters. --//
+		Selector.initialize(Integer.parseInt(tMaxNumGen.getText()));
+		gState = new GameState(1000);
+		train = false;
+		Entity.enableView(true);
+
+		// -- Add the players and the ball to the AnchorPaane. --//
 		players = gState.getPlayers();
 		ball = gState.getBall();
 
@@ -141,10 +180,20 @@ public class SimulationController extends Controller {
 		XYChart.Data<String, Double> yData = new Data<String, Double>(generation, yTeamAvgFitness);
 		XYChart.Data<String, Double> bData = new Data<String, Double>(generation, bTeamAvgFitness);
 
+		ObservableList<XYChart.Data<String, Double>> yDataPoints = s_chart.getData().get(0).getData();
+		ObservableList<XYChart.Data<String, Double>> bDataPoints = s_chart.getData().get(1).getData();
+
 		Platform.runLater(() -> {
 			s_tGen.setText(generation + "");
 			s_chart.getData().get(0).getData().add(yData);
 			s_chart.getData().get(1).getData().add(bData);
+
+			// Check if it is time to move the axis to the right.
+			if (yDataPoints.size() > 20)
+				yDataPoints.remove(0);
+
+			if (bDataPoints.size() > 20)
+				bDataPoints.remove(0);
 		});
 	}
 }
