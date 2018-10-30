262a9b27d537f7e150d124f7659eb680330691f6
philezwini <ssithungu@gmail.com>
2018-10-30 11:43:49 +0200

    Fixed the bug when displaying customAlerts in Controller class.

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -10,47 +10,42 @@ import javafx.stage.Stage;
 
 //Abstract class for storing functions that will be common to different types of controllers.
 public abstract class Controller implements Initializable {
-
-	private static Stage stage;
-
 	// method for handling closing how the program will close.
 	public static void closeWindow(Stage stage, boolean confirmClose) {
-		if(Controller.stage == null)
-			Controller.stage = stage; // Get the scene that contains the node passed as a
-															// parameter.
 		// Check if the program is allowed to close without confirming with the user
 		// first.
 		if (!confirmClose) {
-			Controller.stage.close();
+			stage.close();
 			return; // Return control to the caller.
 		}
-		makeCloseAlert(Controller.stage); // Confirm before closing.
+		makeCloseAlert(stage); // Confirm before closing.
 	}
-	
+
 	// Helper function for first confirming with the user before closing the window.
 	private static void makeCloseAlert(Stage stage) {
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Close Application");
 		alert.setHeaderText("You are about to exit StratFinder.");
 		alert.setContentText("Are you sure you want to exit?");
-		findCenter(alert);
+		findCenter(alert, stage);
 		Optional<ButtonType> result = alert.showAndWait(); // Wait for the user's input before returning control to the
 															// caller.
 		if (result.get() == ButtonType.OK)
 			stage.close();
 	}
 
-	public static void makeCustomAlert(String text) {
-		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("StraFinder");
-		alert.setHeaderText(text);
-		findCenter(alert);
+	public static void makeCustomAlert(Stage stage, String headerText, String contentText) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("StratFinder");
+		alert.setHeaderText(headerText);
+		alert.setContentText(contentText);
+		findCenter(alert, stage);
 		alert.showAndWait();
 	}
 
 	// Helper function for ensuring that the alert box passed as a parameter is
 	// always at the center of the parent window.
-	private static void findCenter(Alert alert) {
+	private static void findCenter(Alert alert, Stage stage) {
 		double x1 = stage.getX();
 		double y1 = stage.getY();
 		double x2 = x1 + stage.getWidth();
